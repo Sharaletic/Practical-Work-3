6e6954cbb28f0bb2f0f60119d0e9feebf8a37f32
Vadim <lyamin.vadim.05@mail.ru>
2024-10-22 23:12:44 +0700

    Добавлен тест для метода parseInt

--- Practic 3/TestLib/LogicTest.cs
+++ Practic 3/TestLib/LogicTest.cs
@@ -13,5 +13,12 @@ namespace TestLib
             Assert.AreEqual(new DateTime(2024, 12, 25), Factory.parseDate("25.12.2024"));
             Assert.ThrowsException<Exception>(() => Factory.parseDate("25.13.2024"));
         }
+
+        [TestMethod]
+        public void parseIntData()
+        {
+            Assert.AreEqual(12, Factory.parseInt("12"));
+            Assert.ThrowsException<Exception>(() => Factory.parseInt("anytext"));
+        }
     }
 }
